721f4cd745ee464d85a64e3dc355270bf5c94991
SachinYadavAug20 <samtagon777@gmail.com>
2026-05-29 12:49:29 +0530

    Basic of edit profile page

---
+++ app/(root)/profile/edit/page.tsx
@@ -0,0 +1,21 @@
+import { auth } from "@/auth";
+import ProfileForm from "@/components/forms/ProfileForm";
+import ROUTES from "@/constant/routes";
+import { IUser } from "@/dataBase/user.model";
+import { getUser } from "@/lib/actions/user.action";
+import { redirect } from "next/navigation";
+
+const page =async () => {
+  const session=await auth();
+  if(!session?.user?.id) redirect(ROUTES.SIGN_IN);
+  const {success,data}=await getUser({userId:session.user.id});
+  if(!success) redirect(ROUTES.SIGN_IN);
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Edit Profile</h1>
+      <ProfileForm user={data?.user as IUser} />
+    </>
+  )
+}
+
+export default page
---
+++ components/forms/ProfileForm.tsx
@@ -0,0 +1,9 @@
+import { IUser } from "@/dataBase/user.model"
+
+const ProfileForm = ({user}:{user:IUser}) => {
+  return (
+    <div>form</div>
+  )
+}
+
+export default ProfileForm
